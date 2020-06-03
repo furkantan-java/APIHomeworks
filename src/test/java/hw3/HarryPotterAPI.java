65f7af54bf01e9d41b1ea6b2f572433de66fc226
furkantan-java <furkan.tan@windowslive.com>
2020-06-03 17:27:07 -0400

    updates

--- src/test/java/hw3/HarryPotterAPI.java
+++ src/test/java/hw3/HarryPotterAPI.java
@@ -15,8 +15,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class HarryPotterAPI {
 
@@ -146,7 +145,7 @@ public class HarryPotterAPI {
                             contentType("application/json; charset=utf-8").
                             body("size()",is(195));
 
-        List<Object>characterList = response.jsonPath().getList("",Object.class);
+        List<Object>characterList = response.jsonPath().getList("");
 
         assertTrue(characterList.size() == 195);
     }
@@ -154,63 +153,225 @@ public class HarryPotterAPI {
 //    Verify number of character id and house
 //1. Send a get request to /characters. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
+//              • Query param key with value {{apiKey}}
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Verify all characters in the response have id field which is not empty
 //4. Verify that value type of the field dumbledoresArmy is a boolean in all characters in the response
 //5. Verify value of the house in all characters in the response is one of the following:
 //            "Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"
+
+    @Test
+    @DisplayName("Verify number of character id and house")
+    public void numOfCharacterIDandHouser() {
+        Response response =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                get("/characters").prettyPeek();
+
+        List<String>houseList = Arrays.asList("Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff");
+
+        response.then().
+                assertThat().
+                        statusCode(200).
+                        contentType("application/json; charset=utf-8").
+                        body("_id",everyItem(not(isEmptyString()))).
+                        body("dumbledoresArmy",everyItem(is(instanceOf(Boolean.class)))).
+                        body("house",everyItem(is(oneOf("Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff",null))));
+
+    }
+
 //    Verify all character information
 //1. Send a get request to /characters. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
+//              • Query param key with value {{apiKey}}
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Select name of any random character
 //4. Send a get request to /characters. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
-//• Query param name with value from step 3
-//            5. Verify that response contains the same character information from step 3. Compare all fields.
+//          • Query param key with value {{apiKey}}
+//          • Query param name with value from step 3
+//5. Verify that response contains the same character information from step 3. Compare all fields.
+    @Test
+    @DisplayName("Verify all character information")
+    public void allCharactersInfo() {
+        Response response =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                when().
+                        get("/characters").prettyPeek();
+
+        response.then().
+                assertThat().
+                        statusCode(200).
+                        contentType("application/json; charset=utf-8");
+
+        List<Map<String,String>>allCharacters = response.jsonPath().getList("");
+
+        System.out.println("allCharacters = " + allCharacters);
+        
+        int randomCharater = new Random().nextInt(allCharacters.size());
+        
+        String anyName = allCharacters.get(randomCharater).get("name");
+
+        System.out.println("anyName = " + anyName);
+
+        Response response2 =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                        queryParam("name",anyName).
+                when().
+                        get("/characters").prettyPeek();
+
+        response2.then().
+                    assertThat().
+                            body("[0].name",is(anyName));
+
+    }
+
+
 //    Verify name search
 //1. Send a get request to /characters. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
-//• Query param name with value Harry Potter
+//              • Query param key with value {{apiKey}}
+//              • Query param name with value Harry Potter
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Verify name Harry Potter
 //4. Send a get request to /characters. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
-//• Query param name with value Marry Potter
+//          • Query param key with value {{apiKey}}
+//          • Query param name with value Marry Potter
 //5. Verify status code 200, content type application/json; charset=utf-8
 //            6. Verify response body is empty
+    @Test
+    @DisplayName("Verify name search")
+    public void nameSearch() {
+
+        Response response =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                        queryParams("name", "Harry Potter").
+                when().
+                        get("/characters").prettyPeek();
+
+        response.then().assertThat().
+                statusCode(200).
+                contentType("application/json; charset=utf-8").
+                body("[0].name",is("Harry Potter"));
+
+        Response response2 =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                        queryParams("name", "Marry Potter").
+                when().
+                        get("/characters").prettyPeek();
+
+        response2.then().assertThat().
+                statusCode(200).
+                contentType("application/json; charset=utf-8").
+                body("[0]",isEmptyOrNullString()).
+                body("size()",is(0));
+
+
+    }
+
 //    Verify house members
 //1. Send a get request to /houses. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
+//              • Query param key with value {{apiKey}}
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Capture the id of the Gryffindor house
 //4. Capture the ids of the all members of the Gryffindor house
 //5. Send a get request to /houses/:id. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
-//• Path param id with value from step 3
-//            6. Verify that response contains the same member ids as the step 4
+//              • Query param key with value {{apiKey}}
+//              • Path param id with value from step 3
+//6. Verify that response contains the same member ids as the step 4
+    @Test
+    @DisplayName("Verify house members")
+    public void houseMember() {
+        Response response =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                when().
+                        get("/houses").prettyPeek();
+
+        response.then().assertThat().
+                    statusCode(200).
+                    contentType("application/json; charset=utf-8");
+
+        String gryffindorID = response.jsonPath().getString("find{it.name == 'Gryffindor'}_id");
+
+        System.out.println("gryffindorID = " + gryffindorID);
+
+        List<String>memberIDs = response.jsonPath().getList("find{it.name == 'Gryffindor'}.members");
+        System.out.println("Gryffindor member IDs = " + memberIDs);
+
+
+        Response response2 =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                        pathParam("id",gryffindorID).
+                when().
+                        get("/houses/{id}").prettyPeek();
+
+        List<String>memberIDsFrom2ndResponse = response2.jsonPath().getList("[0].members._id");
+        System.out.println("memberIDsFrom2ndResponse = " + memberIDsFrom2ndResponse);
+
+        assertEquals(memberIDs,memberIDsFrom2ndResponse);
+
+
+    }
+
 //    Verify house members again
 //1. Send a get request to /houses/:id. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
-//• Path param id with value 5a05e2b252f721a3cf2ea33f
+//              • Query param key with value {{apiKey}}
+//                 • Path param id with value 5a05e2b252f721a3cf2ea33f
 //2. Capture the ids of all members
 //3. Send a get request to /characters. Request includes :
 //            • Header Accept with value application/json
-//• Query param key with value {{apiKey}}
-//• Query param house with value Gryffindor
+//              • Query param key with value {{apiKey}}
+//                  • Query param house with value Gryffindor
 //4. Verify that response contains the same member ids from step 2
+
+    @Test
+    @DisplayName("Verify house members again")
+    public void houseMembers() {
+        Response response =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                        pathParam("id","5a05e2b252f721a3cf2ea33f").
+                when().
+                        get("/houses/{id}").prettyPeek();
+
+        List<String>memberIDs = response.jsonPath().getList("[0].members._id");
+
+        Response response2 =
+                given().
+                        header("Accept","application/json").
+                        queryParams("key",APIKEY).
+                        queryParams("house","Gryffindor").
+                when().
+                        get("/characters").prettyPeek();
+
+        List<String>characterIDs = response.jsonPath().getList("findAll{it.house == 'Gryffindor'}._id");
+
+        assertEquals(memberIDs,characterIDs);
+    }
+
 //    Verify house with most members
 //1. Send a get request to /houses. Request includes :
 //            • Header Accept with value application/json
 //• Query param key with value {{apiKey}}
 //2. Verify status code 200, content type application/json; charset=utf-8
 //            3. Verify that Gryffindor house has the most members
+
 }
--- src/test/java/hw3/pojos/HPCharacter.java
+++ src/test/java/hw3/pojos/HPCharacter.java
@@ -1,46 +1,27 @@
 package hw3.pojos;
-
-
-/**
- * {
- *     "_id": "5a0fa4daae5bc100213c232e",
- *     "name": "Hannah Abbott",
- *     "role": "student",
- *     "house": "Hufflepuff",
- *     "school": "Hogwarts School of Witchcraft and Wizardry",
- *     "__v": 0,
- *     "ministryOfMagic": false,
- *     "orderOfThePhoenix": false,
- *     "dumbledoresArmy": true,
- *     "deathEater": false,
- *     "bloodStatus": "half-blood",
- *     "species": "human"
- * }
- *
- */
-
+import com.google.gson.annotations.SerializedName;
+import hw3.pojos.HPHouse;
+import hw3.pojos.HPMember;
 
 public class HPCharacter {
-    private String _id;
-    private String name;
+    private HPMember member;
     private String role;
-    private String house;
+    private HPHouse house;
     private String school;
-    private Integer __v;
+    @SerializedName("__v")
+    private Integer v;
     private Boolean ministryOfMagic;
     private Boolean orderOfThePhoenix;
     private Boolean dumbledoresArmy;
     private Boolean deathEater;
     private String bloodStatus;
     private String species;
-
-    public HPCharacter(String _id, String name, String role, String house, String school, Integer __v, Boolean ministryOfMagic, Boolean orderOfThePhoenix, Boolean dumbledoresArmy, Boolean deathEater, String bloodStatus, String species) {
-        this._id = _id;
-        this.name = name;
+    public HPCharacter(HPMember member, String role, HPHouse house, String school, Integer v, Boolean ministryOfMagic, Boolean orderOfThePhoenix, Boolean dumbledoresArmy, Boolean deathEater, String bloodStatus, String species) {
+        this.member=member;
         this.role = role;
         this.house = house;
         this.school = school;
-        this.__v = __v;
+        this.v = v;
         this.ministryOfMagic = ministryOfMagic;
         this.orderOfThePhoenix = orderOfThePhoenix;
         this.dumbledoresArmy = dumbledoresArmy;
